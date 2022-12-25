d2441b9105239154f152738ce8a9111d58b4afc2
Zakaria-El-Maachi <zakaria.elmaachi19@gmail.com>
2022-12-25 22:42:13 +0100

    3rd Version

--- src/main/java/com/example/games/Move.java
+++ src/main/java/com/example/games/Move.java
@@ -2,39 +2,19 @@ package com.example.games;
 
 public class Move {
     private Piece piece;
-    private Piece takenPiece;
     private Square start;
     private Square end;
-    private int[] coords;
-
-
-    public Move(Piece piece, Piece takenPiece, Square start, Square end) {
-        this.piece = piece;
-        this.takenPiece = takenPiece;
-        this.start = start;
-        this.end = end;
-    }
-
-    public Move(Piece piece, int[] coords) {
-        this.piece = piece;
-        this.coords = coords;
-
-    }
 
     public Move(Piece piece, Square start, Square end) {
         this.piece = piece;
         this.start = start;
         this.end = end;
-
     }
 
     public Piece getPiece() {
         return piece;
     }
 
-    public Piece getTakenPiece() {
-        return takenPiece;
-    }
 
     public Square getStart() {
         return start;
@@ -44,11 +24,4 @@ public class Move {
         return end;
     }
 
-    public int[] getCoords() {
-        return coords;
-    }
-
-    public void setCoords(int[] coords) {
-        this.coords = coords;
-    }
 }
